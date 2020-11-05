c136103aaa4efa0053843eb9d6ed18ce3ab3ba87
Fanis Michalakis <45755277+fanismichalakis@users.noreply.github.com>
2020-11-05 15:07:15 +0100

    Update ShareActivity.kt
    add button

--- diapo14/ShareActivity.kt
+++ diapo14/ShareActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ShareActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +20,21 @@ class ShareActivity : AppCompatActivity() {
         }else if (intent?.type == "text/plain") {
             Log.d("TAG", intent?.getStringExtra(Intent.EXTRA_TEXT).toString())
             // do stuff with the text
+        }
+
+        button.setOnClickListener {
+            val title: String = resources.getString(R.string.chooser_title)
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Bla bla bla")
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, title)
+
+            if (sendIntent.resolveActivity(packageManager) != null) {
+                startActivity(shareIntent)
+            }
         }
     }
 }
